224a3329261e978125f0b20901721eaff10ef551
amina-2007 <aminagatagova@mail.ru>
2026-04-09 10:29:23 +0300

    hghvfv

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import {ref, computed} from 'vue'
 const movies=ref([
-  {id:1, title:"TWILIGHT",year:2008, rating:6.2, liked:true},
-  {id:2, title:"TWILIGHT NEW MOON",year:2009, rating:8.2, liked:true},
-  {id:3, title:"TWILIGHT ECLIPSE",year:2010, rating:7.2, liked:true},
-  {id:4, title:"TWILIGHT BREAKING DAWN P1",year:2011, rating:3.2, liked:false},
-  {id:5, title:"TWILIGHT BREAKING DAWN P2",year:2012, rating:9, liked:true}
+  {id:1, title:"a",year:2008, rating:6.2, liked:true},
+  {id:2, title:"h",year:2079, rating:8.2, liked:true},
+  {id:3, title:"f",year:4010, rating:7.2, liked:true},
+  {id:4, title:"g",year:2011, rating:3.2, liked:false},
+  {id:5, title:"d",year:2012, rating:9, liked:true}
 ])
 
 const search=ref('');
@@ -13,54 +13,96 @@ const onlyLiked=ref(false);
 const onlyNew=ref(false);
 const sortBy=ref('title');
 
+const error=ref('')
+const newTitle=ref('')
+const newYear=ref('')
+const newRating=ref('')
+
+
 const filteredMovies=computed(()=>{
   const q=search.value.toLowerCase()
   let list=movies.value
   .filter(m=>m.title.toLowerCase().includes(q))
   .filter(m=>!onlyNew.value||m.year>2010)
   .filter(m=>!onlyLiked.value||m.liked)
+
+  if (sortBy.value==='title')
+  {
+    list=[...list].sort((a,b)=>a.title.localeCompare(b.title))
+  }
+  if (sortBy.value==='rating')
+  {
+    list=[...list].sort((a,b)=>b.rating-a.rating);
+  }
+  if (sortBy.value==='year')
+  {
+    list=[...list].sort((a,b)=>b.year-a.year)
+  }
   return list;
 
 })
 
+function toggleLike(movie) 
+{
+movie.liked=!movie.liked;
+}
+
+function removeMovie(id){
+movies.value=movies.value.filter(m=>m.id!==id)
+}
+
+function addMovie()
+{
+  error.value=''
+  if(!newTitle.value)
+  {
+    error.value="INPUT TITLE"
+    return
+  }
+  const id=movies.length===0?1:movies.length+1
+  movies.push({
+    id, 
+    title:newTitle.value,
+    year:newYear.value,
+    rating:newRating.value,
+    liked:false
+  })
+  newTitle.value=''
+  newRating.value=''
+  newYear.value=''
+}
+
+function resetFilter()
+{
+  sortBy.value='tile'
+  search.value=''
+  onlyLiked.value=false
+  onlyNew.value=false
+}
+
 </script>
   
 <template>
+
     <div>
-      <h1>MOVIE CATALOGUE</h1>
-      <p>
-        TOTAL:   |  FEATURED:    |  SHOWED:
-      </p>
-      <input type="text" placeholder="SEARCH" v-model="search">
-      <label>
-<input type="checkbox" v-model="onlyLiked"> FEATURED ONLY 
-      </label>
-
-      <label>
-<input type="checkbox" v-model="onlyNew"> LATER 2010 ONLY 
-      </label>
- 
-      <select >
-        <option >SORTING:TITLE</option>
-        <option >SORTING:YEAR</option>
-        <option >SORTING:RATING</option>
-
-      </select>
-      <button>RESET</button>
+     
       <h3>ADD MOVIE</h3>
 
       <input type="text" placeholder="TITLE">
       <input type="text" placeholder="RATING">
       <input type="text" placeholder="YEAR">
 
-      <button>SUBMIT</button>
+      <button @click="addMovie">SUBMIT</button>
+      <p v-if="error">{{error}}</p>
       <p v-if="filteredMovies.length===0">NO RESULT</p>
       <ul>
-        <li v-for="movie in movies" :key="movie.id">
+        <li v-for="movie in filteredMovies" :key="movie.id">
 {{movie.title}}
 {{movie.year}}
 {{movie.rating}}
-<button @click="click">{{movie.liked ? "👳🏿‍♂️":"🤶🏿"}}</button>
+<button @click="toggleLike(movie)">{{movie.liked ? "👳🏿‍♂️":"🤶🏿"}}</button>
+<button @click="removeMovie(movie.id)">*</button>
+
         </li>
       </ul>
 
---
+++ src/components/filterform.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div>
+      <h1>MOVIE CATALOGUE</h1>
+      <p>
+        TOTAL:   |  FEATURED:    |  SHOWED:
+      </p>
+      <input type="text" placeholder="SEARCH" v-model="search">
+      <label>
+<input type="checkbox" v-model="onlyLiked"> FEATURED ONLY 
+      </label>
+
+      <label>
+<input type="checkbox" v-model="onlyNew"> LATER 2010 ONLY 
+      </label>
+ 
+      <select v-model="sortBy">
+        <option value="title">SORTING:TITLE</option>
+        <option >SORTING:YEAR</option>
+        <option value="rating">SORTING:RATING</option>
+
+      </select>
+      <button>RESET</button>
+       
+      </div> 
+</template>
