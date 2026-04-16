2161c25897bc377ccd27c3a443451ba92c87e20c
amina-2007 <aminagatagova@mail.ru>
2026-04-16 10:33:41 +0300

    v

--- src/views/movieformPAGE.vue
+++ src/views/movieformPAGE.vue
@@ -12,3 +12,26 @@
 />
     </div>
 </template>
+
+<script setup>
+ function addMovie()
+{
+  error.value=''
+  if(!newTitle.value)
+  {
+    error.value="INPUT TITLE"
+    return
+  }
+ const id = movies.value.length === 0 ? 1 : Math.max(...movies.value.map(m => m.id)) + 1
+  movies.value.push({
+   id, 
+  title: newTitle.value,
+  year: Number(newYear.value) || 0,
+  rating: Number(newRating.value) || 0,
+  liked: false
+  })
+  newTitle.value=''
+  newRating.value=''
+  newYear.value=''
+}
+</script>
